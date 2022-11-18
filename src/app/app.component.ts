2707ef5bf85270d91c2b0268c249b32313700332
SspAppunik <shubham.prajapati@appunik.com>
2022-11-18 19:07:37 +0530

    change api data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IModel } from './model.config';
 import { DataService } from './services/data.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,25 +10,23 @@ export class AppComponent implements OnInit {
   title = 'fetch-api-data-testing';
 
   constructor(private service: DataService) {}
-  result: any;
-  result1: any;
-  result2: any;
-  result3: any;
-  result4: any;
+  result!: IModel;
+  result1!: IModel;
+  result2!: IModel;
+  result3!: IModel;
+  result4!: IModel;
  
   
   ngOnInit() {
     this.service.getdata()
     .subscribe(response => {
       console.log(response);
-      // this.result1 = response.data;
-      // this.result = response.data.kpiData;
-      this.result = response.data.kpiData["Does the company have a supplier code of conduct?"][0];
-      this.result1 = response.data.kpiData["Does the company have a supplier code of conduct?"][1];
-      this.result2 = response.data.kpiData["Does the company have a supplier code of conduct?"][2];
-      this.result3 = response.data.kpiData["Does the company have a supplier code of conduct?"][3];
-      this.result4 = response.data.kpiData["Does the company have a supplier code of conduct?"][4];
-    //  this.result1 = response;
+      
+       this.result = response.data.kpiData["Does the company have a supplier code of conduct?"][0];
+       this.result1 = response.data.kpiData["Does the company have a supplier code of conduct?"][1];
+       this.result2 = response.data.kpiData["Does the company have a supplier code of conduct?"][2];
+       this.result3 = response.data.kpiData["Does the company have a supplier code of conduct?"][3];
+       this.result4 = response.data.kpiData["Does the company have a supplier code of conduct?"][4];
     });
 }
 }
---
+++ src/app/model.config.ts
@@ -0,0 +1,17 @@
+export interface IModel {
+    data: any;
+    kpi: string;
+    text_answer: string;
+    text_context: string;
+    text_score: string;
+    score: string;
+    flag: string;
+    uuid:string;
+    validation: string;
+    disclosed: string;
+    captured: string;
+    evidence: string;
+    answer: string;
+    docType: string;
+    modelType: string;
+}
